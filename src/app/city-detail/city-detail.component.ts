139dc86f96478c59cbbc773e445dbc46c869ef04
bart.vanpelt@gmail.com <bart.vanpelt@gmail.com>
2020-06-01 19:28:16 +0200

    Updated rating

--- src/app/city-detail/city-detail.component.ts
+++ src/app/city-detail/city-detail.component.ts
@@ -14,8 +14,8 @@ import { Observable } from 'rxjs';
 export class CityDetailComponent implements OnInit {
 
   //  @Input() city: City;
-  city: City;
-  id: number;
+  city: City = null;
+  id: number = 0;
   restrictions: string;
   @Output() incrementing = new EventEmitter<number>();
   @Output() decrementing = new EventEmitter<number>();
@@ -33,22 +33,24 @@ export class CityDetailComponent implements OnInit {
       .subscribe((route: ParamMap) => {
         console.log('Route: ', route);
         this.id = parseInt(route.get('id'));
-
-        this.cityService.getCity(this.id)
-          .subscribe(
-            (res: City) => {
-              console.log('Received city ', res);
-              // get new list
-              this.city = res;
-            },
-            err => {
-              console.error('Error during get city ', this.id, ': ', err);
-            },
-            () => {
-              console.log('getCity ready');
-            }
-          )
-      })
+        if ((this.id != null) && (this.id > 0)) {
+          this.cityService.getCity(this.id)
+            .subscribe(
+              (res: City) => {
+                console.log('Received city ', res);
+                // get new list
+                this.city = res;
+              },
+              err => {
+                console.error('Error during get city ', this.id, ': ', err);
+                this.city = null;
+              },
+              () => {
+                console.log('getCity ready');
+              }
+            )
+        }
+      });
   }
 
   showHighLights(): boolean {
@@ -57,16 +59,36 @@ export class CityDetailComponent implements OnInit {
 
   incrementRating(): void {
     console.log('increment by 1');
+    this.city.posrating += 1;
     this.incrementing.emit(1);
+    this.updateCity(this.city);
   }
 
   decrementRating(): void {
     console.log('decrement by 1');
+    this.city.negrating += 1;
     this.decrementing.emit(1);
+    this.updateCity(this.city);
   }
 
   order(city: City): void {
     console.log(`Stedentripje geboekt voor: ${city.name}, voor ${city.price} EURO`);
     this.orderService.stream.next(city); // throw event
+  }
+
+  updateCity(city: City): void {
+    console.log('Update city - id: ', city.id, ' name: ', city.name, ' province: ', city.province, ' photo: ', city.photo);
+    this.cityService.updateCity(city)
+      .subscribe(
+        res => {
+          console.log('Updated city ', city.name);
+        },
+        err => {
+          console.error('Error during adding city ', city.name, ': ', err);
+        },
+        () => {
+          console.log('updateCity ready');
+        }
+      )
   }
 }
